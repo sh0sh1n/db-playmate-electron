4ff6a87b9711f9cf9e2b540bf140fb566b892c3d
Shohan Hasan <shohan.hasan@nyu.edu>
2022-07-20 10:41:05 -0400

    maint: clear changelog
    It contained logs for electron react boilerplate.

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 
-const container = document.getElementById('root')!;
+const container = document.documentElement;
 const root = createRoot(container);
 root.render(<App />);
 
